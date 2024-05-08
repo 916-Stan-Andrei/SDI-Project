0e9766ad7e56ebaadec74b4c5bde47dc2c5a9762
916_Stan_Andrei <andrei.bogdan.stan@stud.ubbcluj.ro>
2024-05-08 17:09:46 +0300

    some sort of backend security

--- Backend/sdi-project/src/main/java/com/example/sdiproject/config/CorsConfig.java
+++ Backend/sdi-project/src/main/java/com/example/sdiproject/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.sdiproject.CorsConfig;
+package com.example.sdiproject.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
